be292156903d15bd10dd44f4a30f83f2036864df
jurgeon018 <jurgeon018@gmail.com>
2019-12-07 15:45:44 +0200

    s

--- docker/simple_dj_docker/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/jurgeon018/disk/dev/snippets/docker/simple_dj_docker/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
