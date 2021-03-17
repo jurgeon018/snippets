46ec2ec5fd9343842984da868e90bf695b5ee743
Andrew Mendela <jurgeon018@gmail.com>
2021-03-17 15:57:44 +0000

    s

--- dev/ldp.py
+++
@@ -1,5 +0,0 @@
-
-import ldap
-ad = ldap.initialize("ldap://fpprod.corp")
-x = ad.simple_bind_s("FpprodLdap", "P455word")
-print(x)
--- dev/subnet.py
+++
@@ -1,5 +0,0 @@
-import ipaddress
-
-net4 = ipaddress.ip_network("10.30.26.64/27")
-for host in net4.hosts():
-    print(host)
